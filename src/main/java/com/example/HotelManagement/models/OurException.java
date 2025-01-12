ddf08d21dc9f935095fe9a5c6fce8e8f66fcb926
Mohit Gupta <mohitofficial182@gmail.com>
2025-01-12 22:50:38 +0530

    repository queries added

---
+++ src/main/java/com/example/HotelManagement/models/OurException.java
@@ -0,0 +1,7 @@
+package com.example.HotelManagement.models;
+
+public class OurException extends RuntimeException{
+    public OurException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/example/HotelManagement/respository/RoomRepository.java
+++ src/main/java/com/example/HotelManagement/respository/RoomRepository.java
@@ -1,4 +1,21 @@
 package com.example.HotelManagement.respository;
 
-public class RoomRepository {
+import com.example.HotelManagement.models.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Query(value = "select distinct r.roomType from Room r")
+    List<String> findDistinctRoomTypes();
+
+    @Query("select r from Room r where r.roomType like %:roomType% and r.id NOT in (select bk.room.id from Booking bk where " +
+            "bk.checkInDate <=:checkOutDate and bk.checkOutDate >=:checkInDate) ")
+    List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    @Query(value = "select r from Room r where r.id not in (select b.room.id from Booking b) ")
+    List<Room> getAllAvailableRooms();
 }
